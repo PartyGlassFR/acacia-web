880299bd38d597ac6617e59b6eaa72691c2e85c4
PartyGlassFR <112153063+PartyGlassFR@users.noreply.github.com>
2026-04-18 13:15:24 -0500

    Change CDN URLs from jsdelivr to unpkg
    Updated the handler, bundle, and sw URLs to use unpkg instead of jsdelivr.

--- uv.config.js
+++ uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
     prefix: '/acacia-web/uv/service/',
-    bare: 'https://tomp.app/', // UV v2 connects directly to the server here!
+    bare: 'https://tomp.app/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.handler.js',
-    bundle: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.bundle.js',
+    handler: 'https://unpkg.com/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.handler.js',
+    bundle: 'https://unpkg.com/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.bundle.js',
     config: '/acacia-web/uv.config.js',
-    sw: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.sw.js',
+    sw: 'https://unpkg.com/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.sw.js',
 };
