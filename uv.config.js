1e2ee5f9ee039c74cca48b1d5852d6abb6e0a10c
PartyGlassFR <112153063+PartyGlassFR@users.noreply.github.com>
2026-04-18 13:20:27 -0500

    Change handler and bundle URLs to relative paths
    Updated handler and bundle URLs to relative paths.

--- uv.config.js
+++ uv.config.js
@@ -3,8 +3,8 @@ self.__uv$config = {
     bare: 'https://tomp.app/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: 'https://unpkg.com/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.handler.js',
-    bundle: 'https://unpkg.com/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.bundle.js',
+    handler: '/acacia-web/uv.handler.js',
+    bundle: '/acacia-web/uv.bundle.js',
     config: '/acacia-web/uv.config.js',
-    sw: 'https://unpkg.com/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.sw.js',
+    sw: '/acacia-web/uv.sw.js',
 };
