c9e1a91e251144b4e9f866ad7d0dd037c4aa846b
PartyGlassFR <112153063+PartyGlassFR@users.noreply.github.com>
2026-04-18 12:56:53 -0500

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
     prefix: '/acacia-web/uv/service/',
-    bare: 'https://tomp.app/',
+    bare: 'https://tomp.app/', // UV v2 connects directly to the server here!
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.1/dist/uv.handler.js',
-    bundle: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.1/dist/uv.bundle.js',
+    handler: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.handler.js',
+    bundle: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.bundle.js',
     config: '/acacia-web/uv.config.js',
-    sw: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.1/dist/uv.sw.js',
+    sw: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.sw.js',
 };
