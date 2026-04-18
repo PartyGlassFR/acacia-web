0a88d1d9789b43d972cadb5323e88cb58bb8ced5
PartyGlassFR <112153063+PartyGlassFR@users.noreply.github.com>
2026-04-18 13:45:41 -0500

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/acacia-web/uv/service/',
-    bare: 'https://tomp.app/',
+    bare: 'https://bare.benrogo.net/', // Swapped from tomp.app to a working bridge!
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/acacia-web/uv.handler.js',
