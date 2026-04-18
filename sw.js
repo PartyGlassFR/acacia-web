af2b6fd2a9160f4dec550c6bab5a9495387d5a8d
PartyGlassFR <112153063+PartyGlassFR@users.noreply.github.com>
2026-04-18 13:20:36 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-importScripts('https://unpkg.com/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.bundle.js');
+importScripts('uv.bundle.js');
 importScripts('uv.config.js');
-importScripts('https://unpkg.com/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.sw.js');
+importScripts('uv.sw.js');
 
 const uv = new UVServiceWorker();
 
